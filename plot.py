bd9aa5fff1508aad13bd849274bd5bd91111e74b
Yipeng Zhang <eli@Yipengs-MacBook-Pro.local>
2020-07-11 22:15:12 -0400

    .

--- plot.py
+++ plot.py
@@ -12,13 +12,17 @@ def acc_curve(r_mat):
 plt.style.use('seaborn')
 
 icarl = np.load('Rmatrix_icarl.npy')
-bic = np.load('Rmatrix_bic.npy')
+bic_fc = np.load('Rmatrix_bic_fc.npy')
+ucir_cosine = np.load('Rmatrix_ucir_cosine.npy')
 
 acc_icarl = acc_curve(icarl)
-acc_bic = acc_curve(bic)
+acc_bic_fc = acc_curve(bic_fc)
+acc_ucir_cosine = acc_curve(ucir_cosine)
 
 plt.plot(acc_icarl, label='icarl')
-plt.plot(acc_bic, label='bic')
+plt.plot(acc_bic_fc, label='bic_fc')
+plt.plot(acc_ucir_cosine, label='ucir_cosine')
+plt.plot()
 plt.legend()
 plt.xticks(range(10))
 plt.show()
