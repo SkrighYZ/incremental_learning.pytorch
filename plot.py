a201e5c25e3a39bfe31b12c611ce4a36c2eed4e3
Yipeng Zhang <eli@Yipengs-MacBook-Pro.local>
2020-07-13 01:07:15 -0400

    .

--- plot.py
+++ plot.py
@@ -21,21 +21,25 @@ results_folder = 'results'
 icarl = np.load(os.path.join(results_folder, 'Rmatrix_icarl.npy'))
 bic_fc = np.load(os.path.join(results_folder, 'Rmatrix_bic_fc.npy'))
 ucir_cosine = np.load(os.path.join(results_folder, 'Rmatrix_ucir_cosine.npy'))
+itaml = np.load(os.path.join(results_folder, 'Rmatrix_itaml.npy'))
 
 if sys.argv[1] == 'seen':
 	acc_icarl = acc_seen(icarl)
 	acc_bic_fc = acc_seen(bic_fc)
 	acc_ucir_cosine = acc_seen(ucir_cosine)
+	acc_itaml = acc_seen(itaml)
 	title = 'Accuracy on all seen tasks'
 else:
 	acc_icarl = acc_current(icarl)
 	acc_bic_fc = acc_current(bic_fc)
 	acc_ucir_cosine = acc_current(ucir_cosine)
+	acc_itaml = acc_current(itaml)
 	title = 'Accuracy on current task'
 
 plt.plot(acc_icarl, label='icarl')
-plt.plot(acc_bic_fc, label='bic_fc')
-plt.plot(acc_ucir_cosine, label='ucir_cosine')
+plt.plot(acc_bic_fc, label='bic')
+plt.plot(acc_ucir_cosine, label='ucir')
+plt.plot(acc_itaml, label='itaml')
 plt.plot([9], [0.926], marker='o', markersize=10, label='oracle')
 plt.title(title)
 plt.legend()
